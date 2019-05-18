9bb49b6bcb90028a018d4fc5096ed8436fd0e995
alihoseiny <barzi1375@gmail.com>
2019-05-18 09:32:26 +0430

    Update download url of setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Mohammadreza Alihoseiny',
     author_email='salam@alihoseiny.ir',
     url='https://github.com/alihoseiny/word_cloud_fa',
-    download_url='https://github.com/alihoseiny/word_cloud_fa/archive/0.1.tar.gz',
+    download_url='https://github.com/alihoseiny/word_cloud_fa/archive/0.1.2.tar.gz',
     keywords=['wordcloud', 'word cloud', 'Farsi', 'persian', 'Iran', 'nlp', 'National Language Processing',
               'text processing', 'data visualization'],
     install_requires=[
