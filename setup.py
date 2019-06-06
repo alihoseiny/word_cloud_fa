014c4df7877d7e387112f23b0073c88a4f0ff21e
alihoseiny <barzi1375@gmail.com>
2019-06-06 12:09:46 +0430

    update setpup.py for containing readme

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='wordcloud_fa',
     packages=['wordcloud_fa'],
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='A wrapper for wordcloud module for creating persian word cloud.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Mohammadreza Alihoseiny',
     author_email='salam@alihoseiny.ir',
     url='https://github.com/alihoseiny/word_cloud_fa',
