6a0742b26b35b5b27cf73a181246072aa4b9a273
Mohammadreza Alihoseiny <barzi1375@gmail.com>
2022-10-27 10:16:04 +0330

    Update wordcloud to 1.8.2.2

--- Examples/mixed-example.py
+++ Examples/mixed-example.py
@@ -1,10 +1,12 @@
 from wordcloud_fa import WordCloudFa
 
-wodcloud = WordCloudFa(persian_normalize=True, include_numbers=False, background_color="white")
-text = ""
+wodcloud = WordCloudFa(persian_normalize=True, include_numbers=False, background_color="white", stopwords={'[', ']', '[]', '] [', '[ ]', '] .', ')', '('}, no_reshape=True)
+
 with open('mixed-example.txt', 'r') as file:
     text = file.read()
+
 wc = wodcloud.generate(text)
+print(wc.process_text(text))
 image = wc.to_image()
 image.show()
 image.save('mixed-example.png')
